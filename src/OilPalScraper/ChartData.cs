c5768e1adc0c10b65ac6574ff39b619510346661
Nick.Randell <nick@nickrandell.com>
2016-09-06 16:47:49 +0100

    Oil scraper

---
+++ src/OilPalScraper/ChartData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace OilPalScraper
+{
+    [DataContract]
+    public class ChartData
+    {
+        [DataMember(Name = "Label")]
+        public string[] Label { get; set; }
+
+        [DataMember(Name = "Data")]
+        public string[] Data { get; set; }
+    }
+}
---
+++ src/OilPalScraper/Reading.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OilPalScraper
+{
+    public class Reading
+    {
+        public DateTime TimestampUtc { get; set; }
+        public OilLevel[] Levels { get; set; }
+
+        public override string ToString() => $"{TimestampUtc}: {String.Join<OilLevel>(", ", Levels)}";
+    }
+}
